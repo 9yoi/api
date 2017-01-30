1dc87b5488b520a2bd389af20bb32eef129e91f7
meijiao <meijiao@gmail.com>
2017-01-29 21:55:08 -0800

    Save Phase 1 details into database
    - Endpoint: '/api/user'
    - Takes in JSON from client
    - Parses JSON and saves it into Users table

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const password = require ('./KEYS'); // MySql auth password removed from git repo
-console.log(password, 'pass')
+
 var sequelize = new Sequelize('nova', 'root', password, {
   host: 'localhost',
   dialect: 'mysql',
@@ -15,41 +15,30 @@ var connect = function () {
     }, function (err) { 
       console.log('Unable to connect to the database:', err);
     });
-
-  var User = sequelize.define('user', {
-    userId: {
-      type: Sequelize.INTEGER,
-      unique: true
-    },
-    firstName: {
-      type: Sequelize.STRING,
-    },
-    lastName: {
-      type: Sequelize.STRING
-    },
-    fileType: {
-      type: Sequelize.STRING
-    },
-    fileTag: {
-      type: Sequelize.STRING
-    }
-  }, {
-    freezeTableName: true // Model tableName will be the same as the model name
-  });
-
-  User.sync({force: true}).then(function () {
-    // Table created
-    return User.create({
-      userId: 123,
-      firstName: 'John',
-      lastName: 'Hancock',
-      fileType: 'txt',
-      fileTag: 'mortageß'
-    });
-  });
-  
 }
 
+var User = sequelize.define('user', {
+  userId: {
+    type: Sequelize.INTEGER,
+    unique: true
+  },
+  firstName: {
+    type: Sequelize.STRING,
+  },
+  lastName: {
+    type: Sequelize.STRING
+  },
+  fileType: {
+    type: Sequelize.STRING
+  },
+  fileTag: {
+    type: Sequelize.STRING
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
 module.exports = {
-  connect: connect
+  connect: connect,
+  User: User
 }
