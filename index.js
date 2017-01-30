3d84e007e7f1d33302f6fb083ed50f36cb482e4e
meijiao <meijiao@gmail.com>
2017-01-29 21:55:08 -0800

    Save Phase 1 details into database
    - Endpoint: '/api/user'
    - Takes in JSON from client
    - Parses JSON and saves it into Users table

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require('express')
-const app = express()
-const axios = require('axios');
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const morgan = require('morgan');
 const db = require ('./db.js');
+const User = db.User;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -11,5 +16,25 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
-// connect mySQL and create database           
+// connect mySQL, create database and schema           
 db.connect();
+
+// ENDPOINTS
+
+// Receive Phase 1 data
+app.post('/api/user', function (req, res) {
+  var user = req.body;
+  // For development, drop tables if table already present
+  User.sync({force: true}).then(function () {
+    // Table created
+    return User.create({
+      userId: user.userId,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      fileType: user.fileType,
+      fileTag: user.fileTag
+    });
+  });
+
+  res.send(`Sucess. Saved user: ${user.userId}`);
+})
